7f02572926beaaa1ba11b919d163415a31ea85ba
Konstantin <kkmishukov@gmail.com>
2021-04-24 22:43:10 +0300

    Added Task6

---
+++ Task6/EngDict.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Task6
+{
+    class EngDict: Translator
+    {
+        Dictionary<string, HashSet<string>> _engWords, _ruWords;
+
+        public EngDict()
+        {
+            _engWords = new Dictionary<string, HashSet<string>>
+            {
+                { "hello", new HashSet<string> { "привет", "здравствуй"} },
+                { "bye", new HashSet<string> { "пока" } }
+            };
+
+            _ruWords = new Dictionary<string, HashSet<string>>
+            {
+                { "привет", new HashSet<string> { "hello", "hi", "hey"} },
+                { "пока", new HashSet<string> { "bye" } }
+            };
+        }
+
+
+        public List<string> GetTranslations(string word)
+        {
+            List<string> translations = new List<string>();
+            HashSet<string> words;
+            if (_engWords.TryGetValue(word.ToLower(), out words))
+            {
+                translations = words.ToList();
+            }
+
+            // Распечатка перевода
+            if (translations.Count > 0)
+            {
+                for (int i = 0; i < translations.Count; i++)
+                {
+                    Console.WriteLine(translations[i]);
+                }
+            } else
+            {
+                Console.WriteLine("Данное слово в словаре не найдено.");
+            }
+
+            return translations;
+        }
+
+        public void Add(string word, string translation)
+        {
+            HashSet<string> translations;
+            if (_engWords.TryGetValue(word.ToLower(), out translations)) {
+                translations.Add(translation.ToLower());
+            } else
+            {
+                translations = new HashSet<string>();
+                translations.Add(translation.ToLower());
+                _engWords.Add(word.ToLower(), translations);
+            }
+            Console.WriteLine($"Добавлен перевод \"{translation.ToLower()}\" к слову \"{word.ToLower()}\"");
+            return;
+        }
+
+        public void Add(string word, List<string> translations)
+        {
+            return;
+        }
+      
+    }
+}
---
+++ Task6/Translator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task6
+{
+    interface Translator
+    {
+        void Add(string word, string translation);
+        void Add(string word, List<string> translations);
+        List<string> GetTranslations(string word);
+    }
+}
